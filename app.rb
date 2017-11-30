2e3e02515fe4afdd79ea8a6fd6c750dfbdc06c0c
Mariam Ahhttouche <maryam.ahhtouch@gmail.com>
2017-11-30 18:48:45 +0000

    correction

--- app.rb
+++ app.rb
@@ -39,20 +39,18 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    if letter != nil 
-      #begin
-      #if letter =~ /[[:alpha:]]/
-         @game.guess(letter)
-      #rescue ArgumentError
-      #else
-      #  flash[:message] = 'Invalid guess.'
-      #end
-    end### YOUR CODE HERE ###
-    #flash[:message] = 'Invalid guess.' unless @game.guess(letter).success?
-    #if @game.guess(letter) == false
+    if letter == nil 
+      flash[:message] = 'Invalid guess.'
+      redirect '/show'
+    else
+        if letter =~ /[[:alpha:]]/
+          @game.guess(letter)
+        else
+          flash[:message] = 'Invalid guess.'
+          redirect '/show'
+        end
+    end
     flash[:message] = 'You have already used that letter.' unless @game.guess(letter)
-   # end
-    
     redirect '/show'
   end
  
