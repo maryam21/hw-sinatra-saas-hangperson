b89c0a328c892c21c38e38e5bdbfa52dcdf4a55e
Mariam Ahhttouche <maryam.ahhtouch@gmail.com>
2019-06-29 23:23:04 +0000

    wip

--- spec/hangperson_game_spec.rb
+++ spec/hangperson_game_spec.rb
@@ -8,8 +8,8 @@ describe HangpersonGame do
       game.guess(letter)
     end
   end
-
-  describe 'new', :pending => true do
+ 
+  describe 'new' do
     it "takes a parameter and returns a HangpersonGame object" do      
       @hangpersonGame = HangpersonGame.new('glorp')
       expect(@hangpersonGame).to be_an_instance_of(HangpersonGame)
